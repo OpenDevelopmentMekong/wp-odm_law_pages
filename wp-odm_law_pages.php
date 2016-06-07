c5a30221c297218c5ff97f1c3f4b5ad3697ef9c7
Alex Corbi <a.corbi@gmail.com>
2016-06-07 08:52:13 +0200

    adding css

--- wp-odm_law_pages.php
+++ wp-odm_law_pages.php
@@ -11,11 +11,13 @@
  */
 
 // Require dependencies isntalled via composer
-require get_stylesheet_directory().'/vendor/autoload.php';
+require_once plugin_dir_path( __FILE__ ).'vendor/autoload.php';
 
-class OpenDev_Law_Pages
-{
-    /**
+if (!class_exists('OpenDev_Law_Pages')) {
+
+  class OpenDev_Law_Pages
+  {
+      /**
      * A Unique Identifier.
      */
     protected $plugin_slug;
@@ -35,11 +37,11 @@ class OpenDev_Law_Pages
      */
     public static function get_instance()
     {
-        if (null == self::$instance) {
-            self::$instance = new OpenDev_Law_Pages();
-        }
+      if (null == self::$instance) {
+          self::$instance = new self();
+      }
 
-        return self::$instance;
+      return self::$instance;
     }
 
     /**
@@ -47,88 +49,113 @@ class OpenDev_Law_Pages
      */
     private function __construct()
     {
-        $this->templates = array();
 
-            // Add a filter to the attributes metabox to inject template into the cache.
-            add_filter(
-                'page_attributes_dropdown_pages_args',
-                 array($this, 'register_project_templates')
-            );
+      $this->templates = array();
 
-            // Add a filter to the save post to inject out template into the page cache
-            add_filter(
-                'wp_insert_post_data',
-                array($this, 'register_project_templates')
-            );
+      // Add a filter to the attributes metabox to inject template into the cache.
+      add_filter(
+          'page_attributes_dropdown_pages_args',
+           array($this, 'register_project_templates')
+      );
 
-            // Add a filter to the template include to determine if the page has our
-            // template assigned and return it's path
-            add_filter(
-                'template_include',
-                array($this, 'view_project_template')
-            );
+      // Add a filter to the save post to inject out template into the page cache
+      add_filter(
+          'wp_insert_post_data',
+          array($this, 'register_project_templates')
+      );
 
-            // Add your templates to this array.
-            $this->templates = array('templates/page-laws.php' => 'Law pages',);
+      // Add a filter to the template include to determine if the page has our
+      // template assigned and return it's path
+      add_filter(
+          'template_include',
+          array($this, 'view_project_template')
+      );
+
+      add_action('init', array($this, 'register_styles'));
+
+      // Add your templates to this array.
+      $this->templates = array('templates/page-laws.php' => 'Law pages');
+    }
+
+    function register_styles()
+    {
+      wp_register_style('style',  plugin_dir_url( __FILE__ ).'css/law-pages.css');
+      wp_enqueue_style('style');
+    }
+
+    public static function activate()
+    {
+      // Do nothing
+    }
+
+    public static function deactivate()
+    {
+      // Do nothing
+    }
+
+    /**
+     * Adds our template to the pages cache in order to trick WordPress
+     * into thinking the template file exists where it doens't really exist.
+     */
+    public function register_project_templates($atts)
+    {
+      // Create the key used for the themes cache
+      $cache_key = 'page_templates-'.md5(get_theme_root().'/'.get_stylesheet());
+
+      // Retrieve the cache list.
+      // If it doesn't exist, or it's empty prepare an array
+      $templates = wp_get_theme()->get_page_templates();
+        if (empty($templates)) {
+            $templates = array();
         }
 
-        /**
-         * Adds our template to the pages cache in order to trick WordPress
-         * into thinking the template file exists where it doens't really exist.
-         */
-        public function register_project_templates($atts)
-        {
+      // New cache, therefore remove the old one
+      wp_cache_delete($cache_key, 'themes');
 
-          // Create the key used for the themes cache
-          $cache_key = 'page_templates-'.md5(get_theme_root().'/'.get_stylesheet());
+      // Now add our template to the list of templates by merging our templates
+      // with the existing templates array from the cache.
+      $templates = array_merge($templates, $this->templates);
 
-          // Retrieve the cache list.
-          // If it doesn't exist, or it's empty prepare an array
-          $templates = wp_get_theme()->get_page_templates();
-          if (empty($templates)) {
-              $templates = array();
-          }
+      // Add the modified cache to allow WordPress to pick it up for listing
+      // available templates
+      wp_cache_add($cache_key, $templates, 'themes', 1800);
 
-          // New cache, therefore remove the old one
-          wp_cache_delete($cache_key, 'themes');
+      return $atts;
+    }
 
-          // Now add our template to the list of templates by merging our templates
-          // with the existing templates array from the cache.
-          $templates = array_merge($templates, $this->templates);
+    /**
+     * Checks if the template is assigned to the page.
+     */
+    public function view_project_template($template)
+    {
+      global $post;
 
-          // Add the modified cache to allow WordPress to pick it up for listing
-          // available templates
-          wp_cache_add($cache_key, $templates, 'themes', 1800);
+      if (!isset($post->ID)) {
+          return;
+      }
 
-          return $atts;
-        }
+      if (!isset($this->templates[get_post_meta(
+              $post->ID, '_wp_page_template', true
+          )])) {
+          return $template;
+      }
 
-        /**
-         * Checks if the template is assigned to the page.
-         */
-        public function view_project_template($template)
-        {
-            global $post;
+      $file = plugin_dir_path(__FILE__).get_post_meta(
+          $post->ID, '_wp_page_template', true
+      );
 
-            if (!isset($this->templates[get_post_meta(
-                    $post->ID, '_wp_page_template', true
-                )])) {
-                return $template;
-            }
+      // Just to be safe, we check if the file exist first
+      if (file_exists($file)) {
+          return $file;
+      } else {
+          echo $file;
+      }
 
-            $file = plugin_dir_path(__FILE__).get_post_meta(
-                $post->ID, '_wp_page_template', true
-            );
-
-            // Just to be safe, we check if the file exist first
-            if (file_exists($file)) {
-                return $file;
-            } else {
-                echo $file;
-            }
-
-            return $template;
-        }
+      return $template;
+    }
+  }
 }
 
 add_action('plugins_loaded', array('OpenDev_Law_Pages', 'get_instance'));
+
+?>
