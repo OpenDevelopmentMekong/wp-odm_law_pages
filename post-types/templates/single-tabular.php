b3b5f65416627230a9f68e4893d3434fa8623333
Alex Corbi <a.corbi@gmail.com>
2016-12-08 11:32:45 +0100

    impl-36

--- post-types/templates/single-tabular.php
+++ post-types/templates/single-tabular.php
@@ -236,6 +236,13 @@
 
 <?php get_footer(); ?>
 
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
+<script type="text/javascript">
+  $('select').select2();
+</script>
+
 <script type="text/javascript">
 
 jQuery(document).ready(function($) {
